e519283920498d1eb1924d16c0d17e1b1056166a
irfankhan86004 <iamirfanwebdeveloper@gmail.com>
2016-10-15 13:05:50 +0530

    test commit

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
 
+//echo 'ol';
     protected $table = 'location';
     protected $fillable = [
         'location_name', 'created_at', 'updated_at',
